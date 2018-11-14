37e755b87af43399da9ccd0183ed9a750685b212
Andres Silva <andresilvagomez@gmail.com>
2018-11-14 16:33:27 -0500

    Clase 31

--- PlatziFinanzas/Application/Main/Transactions/TransactionsViewController.swift
+++ PlatziFinanzas/Application/Main/Transactions/TransactionsViewController.swift
@@ -10,22 +10,17 @@ import UIKit
 
 class TransactionsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    
+    fileprivate(set) lazy var emptyStateView: UIView = {
+        guard let view = Bundle.main.loadNibNamed("EmptyState", owner: nil, options: [:])?.first as? UIView  else {
+            return UIView()
+        }
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension TransactionsViewController: UITableViewDelegate {
@@ -34,7 +29,10 @@ extension TransactionsViewController: UITableViewDelegate {
 
 extension TransactionsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        let count = 0
+        tableView.backgroundView = count == 0 ? emptyStateView : nil
+        tableView.separatorStyle = count == 0 ? .none : .singleLine
+        return count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
