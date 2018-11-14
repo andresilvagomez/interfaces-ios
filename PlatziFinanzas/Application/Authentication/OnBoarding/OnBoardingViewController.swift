2f0394c400bb9cc08a27749d136c68412fe600d1
Andres Silva <andresilvagomez@gmail.com>
2018-11-14 14:44:14 -0500

    Clase 24

---
+++ PlatziFinanzas/Application/Authentication/OnBoarding/OnBoardingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  OnBoardingViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Andres Silva on 11/14/18.
+//  Copyright © 2018 Platzi. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingViewController: UIPageViewController {
+    weak var pageControl: UIPageControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
