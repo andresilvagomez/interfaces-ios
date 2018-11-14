b9c1bf07996315fda13c8dfd748865599eebd2d4
Andres Silva <andresilvagomez@gmail.com>
2018-11-14 15:16:11 -0500

    Clase 25

--- PlatziFinanzas/Application/Authentication/OnBoarding/OnBoardingViewController.swift
+++ PlatziFinanzas/Application/Authentication/OnBoarding/OnBoardingViewController.swift
@@ -8,24 +8,79 @@
 
 import UIKit
 
+struct OnBoardingItem {
+    let title: String
+    let description: String
+    let imageName: String
+}
+
 class OnBoardingViewController: UIPageViewController {
-    weak var pageControl: UIPageControl!
+    var pageControl: UIPageControl?
+    
+    fileprivate(set) lazy var items: [OnBoardingItem] = {
+        return [
+            OnBoardingItem(
+                title: "Save money and reduce debt",
+                description: "Press the start button",
+                imageName: "OnBoarding1"
+            ),
+            OnBoardingItem(
+                title: "You´ve finished your onboarding",
+                description: "Press the start button",
+                imageName: "OnBoarding2"
+            )
+        ]
+    }()
+    
+    fileprivate(set) lazy var contentViewController: [UIViewController] = {
+        var items = [UIViewController]()
+        for i in 0 ..< self.items.count {
+            items.append(self.instanteViewController(i))
+        }
+        return items
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        delegate = self
+        dataSource = self
+    }
+
+    func instanteViewController(_ index: Int) -> UIViewController {
+        guard let viewController = UIStoryboard(name: "OBoarding", bundle: Bundle.main)
+            .instantiateViewController(withIdentifier: "OnBoardingSteps") as? OnBoardingStepsViewController else {
+                return UIViewController()
+        }
+        
+        viewController.item = items[index]
+        return viewController
+    }
+}
+
+extension OnBoardingViewController: UIPageViewControllerDataSource {
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        let index = contentViewController.index(of: viewController)
+        if index == 0 {
+            return nil
+        }
+        return contentViewController[index! + 1]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let index = contentViewController.index(of: viewController)
+        if index == contentViewController.count - 1 {
+            return nil
+        }
+        return contentViewController[index! + 1]
     }
-    */
+}
 
+extension OnBoardingViewController: UIPageViewControllerDelegate {
+    
 }
