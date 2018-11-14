42e8824a6fa895bfdb8a5a911bfbfaa8f6d82a5d
Andres Silva <andresilvagomez@gmail.com>
2018-11-14 16:06:40 -0500

    Clase 29

---
+++ PlatziFinanzas/Application/Main/Transactions/TransactionsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TransactionsViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Andres Silva on 11/14/18.
+//  Copyright © 2018 Platzi. All rights reserved.
+//
+
+import UIKit
+
+class TransactionsViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension TransactionsViewController: UITableViewDelegate {
+    
+}
+
+extension TransactionsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    }
+}
