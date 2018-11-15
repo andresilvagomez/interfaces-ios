84131b4d70ac5a6abe3927cf88380b73c19dbe8f
Andres Silva <andresilvagomez@gmail.com>
2018-11-15 08:40:46 -0500

    Clase 35

--- PlatziFinanzas/Application/Main/Budget/BudgetViewController.swift
+++ PlatziFinanzas/Application/Main/Budget/BudgetViewController.swift
@@ -1,40 +1,31 @@
 //
-//  TransactionsViewController.swift
+//  BudgetViewController.swift
 //  PlatziFinanzas
 //
-//  Created by Andres Silva on 11/14/18.
+//  Created by Andres Silva on 11/15/18.
 //  Copyright © 2018 Platzi. All rights reserved.
 //
 
 import UIKit
 
-class TransactionsViewController: UIViewController {
+class BudgetViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    
-    fileprivate(set) lazy var emptyStateView: UIView = {
-        guard let view = Bundle.main.loadNibNamed("EmptyState", owner: nil, options: [:])?.first as? UIView  else {
-            return UIView()
-        }
-        return view
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let cell = UINib(nibName: "TransactionsCell", bundle: Bundle.main)
         tableView.register(cell, forCellReuseIdentifier: "cell")
     }
 }
 
-extension TransactionsViewController: UITableViewDelegate {
+extension BudgetViewController: UITableViewDelegate {
     
 }
 
-extension TransactionsViewController: UITableViewDataSource {
+extension BudgetViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let count = 10
-        tableView.backgroundView = count == 0 ? emptyStateView : nil
-        tableView.separatorStyle = count == 0 ? .none : .singleLine
         return count
     }
     
